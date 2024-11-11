72e29a0d59478fab692b751cfd1ee2ec45106ff3
Jairo Duarte Ribeiro <jairoribeiro.dev@gmail.com>
2024-11-11 18:42:58 +0000

    Add inflation rate as a constant

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,13 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	investmentAmount := 1000.0
 	expectedReturnRate := 5.5
 	years := 10
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
